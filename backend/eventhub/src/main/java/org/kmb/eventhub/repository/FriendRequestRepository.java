fa1302443f407c7ed30bb02a1d8a94b5bc7b8807
Eset Shabakhov <eset-2000@mail.ru>
2025-04-11 01:05:53 +0500

    Рефаккторинг управления друзьями

--- backend/eventhub/src/main/java/org/kmb/eventhub/repository/FriendRequestRepository.java
+++ backend/eventhub/src/main/java/org/kmb/eventhub/repository/FriendRequestRepository.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 import static org.kmb.eventhub.Tables.FRIEND_REQUEST;
-import static org.kmb.eventhub.Tables.USER;
 
 @Repository
 @AllArgsConstructor
@@ -17,11 +16,21 @@ public class FriendRequestRepository {
 
     private final DSLContext dslContext;
 
-    public List<FriendRequest> fetchOptionalBySenderIdOrRecipientId(Long id, Integer page, Integer pageSize) {
+    public List<FriendRequest> fetchOptionalBySenderIdOrRecipientId(Long idFrom, Long idTo, Integer page, Integer pageSize) {
         return dslContext
                 .selectFrom(FRIEND_REQUEST)
-                .where(FRIEND_REQUEST.SENDER_ID.eq(id))
-                .or(FRIEND_REQUEST.RECIPIENT_ID.eq(id))
+                .where(FRIEND_REQUEST.SENDER_ID.eq(idFrom))
+                .or(FRIEND_REQUEST.RECIPIENT_ID.eq(idTo))
+                .limit(pageSize)
+                .offset((page - 1) * pageSize)
+                .fetchInto(FriendRequest.class);
+    }
+
+    public List<FriendRequest> fetchOptionalBySenderIdAndRecipientId(Long idFrom, Long idTo, Integer page, Integer pageSize) {
+        return dslContext
+                .selectFrom(FRIEND_REQUEST)
+                .where(FRIEND_REQUEST.SENDER_ID.eq(idFrom))
+                .and(FRIEND_REQUEST.RECIPIENT_ID.eq(idTo))
                 .limit(pageSize)
                 .offset((page - 1) * pageSize)
                 .fetchInto(FriendRequest.class);
--- backend/eventhub/src/main/java/org/kmb/eventhub/service/FriendService.java
+++ backend/eventhub/src/main/java/org/kmb/eventhub/service/FriendService.java
@@ -1,15 +1,17 @@
 package org.kmb.eventhub.service;
 
 import lombok.AllArgsConstructor;
-import org.jooq.Condition;
+import org.jooq.*;
+import org.jooq.exception.DataTypeException;
 import org.kmb.eventhub.dto.ResponseList;
 import org.kmb.eventhub.enums.FriendRequestStatusType;
 import org.kmb.eventhub.enums.RoleType;
+import org.kmb.eventhub.exception.FriendRequestException;
 import org.kmb.eventhub.exception.UnexpectedException;
 import org.kmb.eventhub.exception.UserNotFoundException;
+import org.kmb.eventhub.exception.UserSelfException;
 import org.kmb.eventhub.repository.FriendRequestRepository;
 import org.kmb.eventhub.tables.daos.FriendRequestDao;
-import org.kmb.eventhub.tables.daos.UserDao;
 import org.kmb.eventhub.tables.pojos.FriendRequest;
 import org.kmb.eventhub.tables.pojos.User;
 import org.springframework.stereotype.Service;
@@ -17,10 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 import static org.jooq.impl.DSL.trueCondition;
-import static org.kmb.eventhub.Tables.USER;
 
 @Service
 @AllArgsConstructor
@@ -32,7 +32,7 @@ public class FriendService {
 
     private final UserService userService;
 
-    private static final String MEMBER_EXPECTED = "Ожидался тип пользователя MEMBER";
+    private static final String MEMBER_EXPECTED = "User role expected MEMBER";
 
     public ResponseList<FriendRequest> getFriendRequestList(Long id, Integer page, Integer pageSize) {
         if (Objects.isNull(userService.get(id)))
@@ -41,7 +41,7 @@ public class FriendService {
         ResponseList<FriendRequest> responseList = new ResponseList<>();
         Condition condition = trueCondition();
 
-        List<FriendRequest> list =  friendRequestRepository.fetchOptionalBySenderIdOrRecipientId(id, page, pageSize);
+        List<FriendRequest> list =  friendRequestRepository.fetchOptionalBySenderIdOrRecipientId(id, id, page, pageSize);
 
         responseList.setList(list);
         responseList.setTotal(friendRequestRepository.count(condition));
@@ -51,12 +51,32 @@ public class FriendService {
     }
 
     @Transactional
-    public void sendFriendRequest(Long userIdFrom, String usernameTo) {
+    public void sendFriendRequest(Long userIdFrom, Long userIdTo) {
 
-        User userTo = userService.getByUsername(usernameTo);
+        if (Objects.equals(userIdFrom, userIdTo))
+            throw new UserSelfException(userIdFrom);
+
+        userService.get(userIdFrom);
+        User userTo = userService.get(userIdTo);
 
         if (!userTo.getRole().equals(RoleType.MEMBER))
             throw new UnexpectedException(MEMBER_EXPECTED);
+        List<FriendRequest> firstList = friendRequestRepository.fetchOptionalBySenderIdAndRecipientId(userIdFrom, userIdTo, 1, 10);
+        List<FriendRequest> secondList = friendRequestRepository.fetchOptionalBySenderIdAndRecipientId(userIdTo, userIdFrom, 1, 10);
+        if (firstList.size() > 1 || secondList.size() > 1)
+            throw new UnexpectedException("More than one relation between users found");
+        firstList.forEach(e -> {
+            if (e.getStatus() == FriendRequestStatusType.ACCEPTED)
+                throw new FriendRequestException(String.format("User with id %d and the user with id %d are friends", userIdFrom, userTo.getId()));
+            if (e.getStatus() == FriendRequestStatusType.PENDING)
+                throw new FriendRequestException(String.format("The user with id %d sent request to the user with id %d", userIdFrom, userTo.getId()));
+        });
+        secondList.forEach(e -> {
+            if (e.getStatus() == FriendRequestStatusType.ACCEPTED)
+                throw new FriendRequestException(String.format("User with id %d and the user with id %d are friends", userTo.getId(), userIdFrom));
+            if (e.getStatus() == FriendRequestStatusType.PENDING)
+                throw new FriendRequestException(String.format("The user with id %d sent request to the user with id %d", userTo.getId(), userIdFrom));
+        });
 
         FriendRequest friendRequest = new FriendRequest();
         friendRequest.setSenderId(userIdFrom);
@@ -66,32 +86,64 @@ public class FriendService {
     }
 
     @Transactional
-    public void acceptFriendRequest(Long idFrom, User userTo) {
+    public void acceptFriendRequest(Long userIdFrom, Long userIdTo) {
 
-        User userFrom = userService.get(idFrom);
+        if (Objects.equals(userIdFrom, userIdTo))
+            throw new UserSelfException(userIdFrom);
 
-        if (!userFrom.getRole().equals(RoleType.MEMBER))
+        userService.get(userIdFrom);
+        User userTo = userService.get(userIdTo);
+
+        if (!userTo.getRole().equals(RoleType.MEMBER))
             throw new UnexpectedException(MEMBER_EXPECTED);
+        List<FriendRequest> firstList = friendRequestRepository.fetchOptionalBySenderIdAndRecipientId(userIdFrom, userIdTo, 1, 10);
+        List<FriendRequest> secondList = friendRequestRepository.fetchOptionalBySenderIdAndRecipientId(userIdTo, userIdFrom, 1, 10);
+        if (firstList.isEmpty())
+            throw new UnexpectedException(String.format("User with id %d did not send a request to user with id %d", userIdFrom, userTo.getId()));
+        if (firstList.size() > 1 || secondList.size() > 1)
+            throw new UnexpectedException("More than one relation between users found");
+        firstList.forEach(e -> {
+            if (e.getStatus() == FriendRequestStatusType.ACCEPTED)
+                throw new FriendRequestException(String.format("User with id %d and the user with id %d are friends", userIdFrom, userIdTo));
+        });
 
         FriendRequest friendRequest = new FriendRequest();
-        friendRequest.setSenderId(userFrom.getId());
+        friendRequest.setSenderId(userIdFrom);
         friendRequest.setRecipientId(userTo.getId());
         friendRequest.setStatus(FriendRequestStatusType.ACCEPTED);
-        friendRequestDao.insert(friendRequest);
+        friendRequestDao.update(friendRequest);
     }
 
     @Transactional
-    public void rejectFriendRequest(Long idFrom, User userTo) {
+    public void rejectFriendRequest(Long userIdFrom, Long userIdTo) {
 
-        User userFrom = userService.get(idFrom);
+        if (Objects.equals(userIdFrom, userIdTo))
+            throw new UserSelfException(userIdFrom);
 
-        if (!userFrom.getRole().equals(RoleType.MEMBER))
+        userService.get(userIdFrom);
+        User userTo = userService.get(userIdTo);
+
+        if (!userTo.getRole().equals(RoleType.MEMBER))
             throw new UnexpectedException(MEMBER_EXPECTED);
+        List<FriendRequest> firstList = friendRequestRepository.fetchOptionalBySenderIdAndRecipientId(userIdFrom, userIdTo, 1, 10);
+        List<FriendRequest> secondList = friendRequestRepository.fetchOptionalBySenderIdAndRecipientId(userIdTo, userIdFrom, 1, 10);
+        if (firstList.isEmpty())
+            throw new UnexpectedException(String.format("User with id %d did not send a request to user with id %d", userIdFrom, userTo.getId()));
+        if (firstList.size() > 1 || secondList.size() > 1)
+            throw new UnexpectedException("More than one relation between users found");
+        firstList.forEach(e -> {
+            if (e.getStatus() == FriendRequestStatusType.ACCEPTED)
+                throw new FriendRequestException(String.format("User with id %d and the user with id %d are friends", userIdFrom, userIdTo));
+            /*if (e.getStatus() == FriendRequestStatusType.REJECTED)
+                throw new FriendRequestException(String.format("User with id %d rejected the user with id %d", userIdTo, userIdFrom));*/
+        });
 
         FriendRequest friendRequest = new FriendRequest();
-        friendRequest.setSenderId(userFrom.getId());
+        friendRequest.setSenderId(userIdFrom);
         friendRequest.setRecipientId(userTo.getId());
-        friendRequest.setStatus(FriendRequestStatusType.REJECTED);
-        friendRequestDao.insert(friendRequest);
+        //friendRequest.setStatus(FriendRequestStatusType.REJECTED);
+        friendRequestDao.update(friendRequest);
+
+        //friendRequestDao.deleteById(userIdFrom);
     }
 }
