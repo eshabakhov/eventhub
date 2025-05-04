21a91cf29b57b1b35c41632c1162f0164e60cbb2
a1tta6 <minigan22@gmail.com>
2025-05-04 16:42:21 +0500

    Чуть-чуть рефакторинга в eventах

--- backend/eventhub/src/main/java/org/kmb/eventhub/event/controller/EventController.java
+++ backend/eventhub/src/main/java/org/kmb/eventhub/event/controller/EventController.java
@@ -95,21 +95,21 @@ public class EventController {
         return eventService.update(id, eventDTO);
     }
 
-//    @Operation(summary = "Удалить мероприятие.",
-//            description = "Удаляет мероприятие по ID.")
-//    @ApiResponse(responseCode = "200",
-//            description = "Мероприятие удалено.",
-//            content = @Content(mediaType = "application/json",
-//            schema = @Schema(implementation = Event.class)))
-//    @ApiResponse(responseCode = "404",
-//            description = "Мероприятие не найдено",
-//            content = @Content(mediaType = "application/json",
-//            schema = @Schema(implementation = ResponseDTO.class)))
-//    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-//    @DeleteMapping(value = "/{id}")
-//    public Long delete(@PathVariable Long id) {
-//        return eventService.delete(id);
-//    }
+    @Operation(summary = "Удалить мероприятие.",
+            description = "Удаляет мероприятие по ID.")
+    @ApiResponse(responseCode = "200",
+            description = "Мероприятие удалено.",
+            content = @Content(mediaType = "application/json",
+            schema = @Schema(implementation = Event.class)))
+    @ApiResponse(responseCode = "404",
+            description = "Мероприятие не найдено",
+            content = @Content(mediaType = "application/json",
+            schema = @Schema(implementation = ResponseDTO.class)))
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{id}")
+    public Long delete(@PathVariable Long id) {
+        return eventService.delete(id);
+    }
 
     @Operation(summary = "Добавление нового файла.",
             description = "Добавляет новый файл к событию.")
@@ -159,24 +159,8 @@ public class EventController {
             @RequestParam(value = "page", defaultValue = "1") Integer page,
             @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
             @RequestParam(value = "search", required = false) String search,
-            @RequestParam(value = "tags", required = false) String tags) {
+            @RequestParam(value = "tags", required = false) List<String> tags) {
 
         return eventService.getListByOrganizerId(page, pageSize, search, tags, id);
-    }
-
-    @Operation(summary = "Удалить мероприятие.",
-            description = "Удаляет мероприятие по ID.")
-    @ApiResponse(responseCode = "200",
-            description = "Мероприятие удалено.",
-            content = @Content(mediaType = "application/json",
-                    schema = @Schema(implementation = Event.class)))
-    @ApiResponse(responseCode = "404",
-            description = "Мероприятие не найдено",
-            content = @Content(mediaType = "application/json",
-                    schema = @Schema(implementation = ResponseDTO.class)))
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    @DeleteMapping(value = "/organizers/{id}/events")
-    public Long deleteOrganizerEvent(@PathVariable Long id, @RequestParam Long eventId) {
-        return eventService.delete(id, eventId);
     }
 }
